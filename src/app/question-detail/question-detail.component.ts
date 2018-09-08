d6acd19d9c31e298ab3d1bfc451785d2e73ebf25
Simon <everett.blake@gmail.com>
2018-09-08 13:43:41 -0600

    update

--- src/app/question-detail/question-detail.component.ts
+++ src/app/question-detail/question-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Question } from '../question';
+
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class QuestionDetailComponent implements OnInit {
   @Input()
-  question;
+  question: Question;
+  difficulty = {
+    1: "easy",
+    2: "medium",
+    3: "hard",
+  }
 
   response: string;
 
